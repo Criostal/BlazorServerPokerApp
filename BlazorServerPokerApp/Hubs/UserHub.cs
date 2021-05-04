995ea4d9df7b74a95642097f55196bc02730c942
Chris Curth <christian.curth@nexus-ag.de>
2021-05-04 09:04:02 +0200

    Add project files.

---
+++ BlazorServerPokerApp/Hubs/UserHub.cs
@@ -0,0 +1,17 @@
+﻿using BlazorServerPokerApp.Data;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerPokerApp.Hubs
+{
+    public class UserHub : Hub
+    {
+        public async Task SendMesage( User user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
